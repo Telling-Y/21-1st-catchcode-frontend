b43d099b99c0b0e3661b25365a3b787f5827734b
무제 <mint1480123@gmail.com>
2021-06-11 19:33:19 +0900

    for checkout

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -3,12 +3,16 @@ import React from 'react';
 class Detail extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      result=[],
+    };
   }
   render() {
     return (
       <div className="detailPageWrap">
         <div className="leftPage">
+          this
+          
           <div className="imgMap">
             <img src="s" alt="junk" />
           </div>
