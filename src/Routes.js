49d971a05291448302e8a58389b6b24b29613ff9
무제 <mint1480123@gmail.com>
2021-06-11 17:06:27 +0900

    Ref:라우터 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import Sign from './Pages/Sign/Sign';
+
 import Nav from './Components/Nav/Nav';
-// import Login from './Pages/Login/Login';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -11,7 +13,9 @@ class Routes extends React.Component {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path="/Main" component={Main} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Sign" component={Sign} />
         </Switch>
       </Router>
     );
