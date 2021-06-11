c856760c4735a3614f6f99d87487208ea2cf9385
무제 <mint1480123@gmail.com>
2021-06-11 17:49:52 +0900

    test

---
+++ src/Pages/Detail/Detail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="detailPageWrap">
+      <div className="leftPage">
+        <div className=""
+        <img src="s" alt="junk"/>
+
+      </div>
+      <div className="rightPage">
+
+      </div>
+    </div>;
+  }
+}
+
+export default Detail;
--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
+import Sign from './Pages/Sign/Sign';
 import Nav from './Component/Nav/Nav';
+import Detail from './Pages/';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
+          <Route exact path="/Main" component={Main} />
           <Route exact path="/Login" component={Login} />
-          <Route exact path="/" component={Nav} />
+          <Route exact path="/Sign" component={Sign} />
         </Switch>
       </Router>
     );
