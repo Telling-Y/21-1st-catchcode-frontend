eb3343b33638a213f8b8e69ef2422fa1005561eb
무제 <mint1480123@gmail.com>
2021-06-15 15:28:49 +0900

    for checkout

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -13,6 +13,7 @@ class Main extends React.Component {
       <>
         <Banner />
         <main className="productArea">
+          this.state.
           <MainProductItem />
         </main>
       </>
