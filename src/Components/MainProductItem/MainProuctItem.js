2692a3948b3e153d0a64b096f7c7e1c2f5efb180
무제 <mint1480123@gmail.com>
2021-06-16 10:48:01 +0900

    checkout

--- src/Components/MainProductItem/MainProuctItem.js
+++ src/Components/MainProductItem/MainProuctItem.js
@@ -9,6 +9,7 @@ class MainProductItem extends Component {
           src="https://media.catchfashion.com/images/h_l?url=https%3A%2F%2Fcdn-images.farfetch-contents.com%2F16%2F06%2F99%2F99%2F16069999_32492034_1000.jpg&t=pl"
           alt="test"
         />
+
         <div className="productName">testName</div>
         <div className="productPrice">10,000</div>
         <div className="productLabel">
