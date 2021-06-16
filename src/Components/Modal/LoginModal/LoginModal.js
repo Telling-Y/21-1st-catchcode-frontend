1cac2ce7fe74db611677cd9f7cb99e73ef3ead71
무제 <mint1480123@gmail.com>
2021-06-16 15:29:59 +0900

    checkout

--- src/Components/Modal/LoginModal/LoginModal.js
+++ src/Components/Modal/LoginModal/LoginModal.js
@@ -3,12 +3,16 @@ import './LoginModal.scss';
 
 class LoginModal extends Component {
   moveToLoginPage = () => {
-    this.props.history.push(`/product/Login`);
+    // this.state.history.push('/Login');
+    console.log(this.props.history);
   };
+
   render() {
     return (
       <div className="loginModal">
-        <button className="mvToSignIn">로그인</button>
+        <button className="mvToSignIn" onClick={() => this.moveToLoginPage()}>
+          로그인
+        </button>
         <button className="mvToSignup">회원가입</button>
       </div>
     );
