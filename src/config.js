44ff3de7c77198465b339a0812051629757bce89
BG-YU <byunggun.yu@gmail.com>
2021-06-17 16:14:55 +0900

    remove config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = '10.58.2.153';
-
-export const POST_LOGIN_API = `http://${BASE_URL}:8000/users/signin`;
-export const POST_SIGN_API = `http://${BASE_URL}:8000/users/signup`;
-export const GET_COTEGORIES_API = `http://${BASE_URL}:8000/products/categories`;
-export const GET_FILTERPRODUCTS_API = `http://${BASE_URL}:8000/products/search`;
-export const GET_BASE_API = `http://${BASE_URL}:8000/prodcuts/`;
