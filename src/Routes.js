6856118ec6984c3d3c4ab3dcf4dc5f1ece71c4ff
BG-YU <byunggun.yu@gmail.com>
2021-06-17 16:00:06 +0900

    ref : conflict fix

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Main from './Pages/Main/Main';
+// import Nav from './Component/Nav/Nav';
+// import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import Sign from './Pages/Sign/Sign';
-// import Nav from './Component/Nav/Nav';
-import Detail from './Pages/Detail/Detail';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Routes extends React.Component {
@@ -12,9 +11,9 @@ class Routes extends React.Component {
       <Router>
         {/* <Nav /> */}
         <Switch>
-          {/* <Route exact path="/" component={Main} /> */}
           <Route exact path="/Login" component={Login} />
           <Route exact path="/Sign" component={Sign} />
+          {/* <Route exact path="/*" component={Main} /> */}
         </Switch>
       </Router>
     );
