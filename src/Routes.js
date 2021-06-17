815c6ec18e942f3c41382074898bc4ee1f4d6f99
Telling-Y <mint1480123@gmail.com>
2021-06-17 18:39:52 +0900

    checkout

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Nav from './Component/Nav/Nav';
-// import Main from './Pages/Main/Main';
+import Nav from './Components/Nav/Nav';
+import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import Sign from './Pages/Sign/Sign';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -9,11 +9,11 @@ class Routes extends React.Component {
   render() {
     return (
       <Router>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
           <Route exact path="/Login" component={Login} />
           <Route exact path="/Sign" component={Sign} />
-          {/* <Route exact path="/*" component={Main} /> */}
+          <Route exact path="/*" component={Main} />
         </Switch>
       </Router>
     );
