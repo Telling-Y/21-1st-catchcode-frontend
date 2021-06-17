46083f0998f5306193a017db433a014c22b5e555
Telling-Y <mint1480123@gmail.com>
2021-06-17 21:01:21 +0900

    Ref : nav fianl

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import Nav from './Components/Nav/Nav';
+// import Nav from './Component/Nav/Nav';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import Sign from './Pages/Sign/Sign';
+import Detail from './Pages/Detail/Detail';
+import FilterPage from './Pages/FilterPage/FilterPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
           <Route exact path="/Login" component={Login} />
           <Route exact path="/Sign" component={Sign} />
+          <Route exact path="/products" component={FilterPage} />
+          <Route exact path="/products/:id" component={Detail} />
           <Route exact path="/*" component={Main} />
         </Switch>
       </Router>
