aa7412df69ed21b3e6c1fae2007b3ea534a152c1
UlongChaS2 <jyujung2@gmail.com>
2021-06-18 19:16:40 +0900

    Modify: config.js contents

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-const BASE_URL = '<http://10.58.5.151:8000>';
+const BASE_URL = 'http://10.58.6.177:8000';
 
 export const GET_LOGIN_API = `${BASE_URL}/users/signin`;
 export const POST_SIGN_API = `${BASE_URL}/users/signup`;
-export const GET_PRODUCT_API = `${BASE_URL}/products`;
+export const GET_PRODUCTS_API = `${BASE_URL}/products/${id}`;
+export const GET_COTEGORIES_API = `${BASE_URL}/products/categories`;
+export const GET_FILTERPRODUCTS_API = `${BASE_URL}/products/search`;
 
 // import { GET_PRODUCT_API } from '../../../config.js';
 // fetch(`${GET_PRODUCT_API}/5`).then(...).then(...);
